8eede9d257a1275cf5ee7aa5b807ab6ae75d98f9
NatChotPai <145831360+NatChotPai@users.noreply.github.com>
2024-11-15 09:43:44 +0700

    First-init-UI-Navigation
    First-init-UI-Navigation

---
+++ admin-dashboard/src/Components/NavigationTemplate/Navigation.jsx
@@ -0,0 +1,18 @@
+import './Navigation.css'
+
+const Navigation = () => {
+  return (
+    <div className='navigation'>
+        <header>
+            <div className="profile">
+                <img src="https://media.istockphoto.com/id/693085478/th/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2/%E0%B8%8A%E0%B8%B2%E0%B8%A2%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%AB%E0%B8%A5%E0%B9%88%E0%B8%AD%E0%B8%A1%E0%B8%B5%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B8%A1%E0%B8%AD%E0%B8%87%E0%B8%81%E0%B8%A5%E0%B9%89%E0%B8%AD%E0%B8%87%E0%B8%94%E0%B9%89%E0%B8%A7%E0%B8%A2%E0%B9%81%E0%B8%82%E0%B8%99%E0%B9%84%E0%B8%82%E0%B8%A7%E0%B9%89.jpg?s=2048x2048&w=is&k=20&c=4vm3-I8TSiBZqz6pKAYZh7N3BxOJAz66pEUrruTGQUE=" 
+                     alt="user-img" 
+                     className="profile-img" />
+            </div>
+            <span>creative_ambition</span>
+        </header>
+    </div>
+  )
+}
+
+export default Navigation
